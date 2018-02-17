bbaaab2d2ba99a4b6e44c90fdcaf3a41ccfc2645
Manuel Schächinger <manuel@schaechinger.com>
2018-02-17 16:32:26 +0100

    - added basic content for address handling

---
+++ test/Address/Addres.test.js
@@ -0,0 +1,37 @@
+const Address = require('../../lib/Address'),
+  errors = require('../../lib/errors');
+
+describe('Address', () => {
+  it('should validate the country code', () => {
+    [
+      'DEU',
+      'MEX'
+    ].forEach(country => {
+      const address = new Address({ country });
+      address._country.should.equal(country);
+    });
+    
+    const address = new Address({ country: null });
+    address._country.should.equal('DEU');
+
+    const INVALID_CODE = 'INVALID';
+    (() => {
+      new Address({ country: INVALID_CODE });
+    }).should.throw(errors.usage.invalidCountryCode + INVALID_CODE);
+  });
+
+  it('should convert address data of normal addresses', () => {
+    const address = new Address({
+      street: 'Marienplatz',
+      houseNo: '1',
+      zip: '80331',
+      city: 'München'
+    });
+
+    const data = address.getData();
+    data.should.have.property('address');
+    data.should.not.have.property('name');
+    data.address.should.have.keys('street', 'houseNo', 'zip', 'city', 'country');
+    data.address.should.not.have.property('additional');
+  });
+});
