42dcb48a3a05e0571f95a132fb9af9add1d45963
Manuel Schächinger <manuel@schaechinger.com>
2018-03-27 21:43:30 +0200

    added product list tests

--- test/ProductList/ProductList.test.js
+++ test/ProductList/ProductList.test.js
@@ -1,17 +1,12 @@
 const ProductList = require('../../lib/ProductList');
 
+const TEST_DATA = require('./ProductList.data.json');
+
 describe('ProductList', () => {
   describe('init', () => {
     const productList = new ProductList({ client: {} });
 
     productList.loadProducts = sinon.stub().returns(Promise.resolve('LOADING'));
-
-    const data = {
-      date: '2018-03-01',
-      salesProductList: {
-        SalesProduct: []
-      }
-    };
 
     beforeEach(() => {
       productList.loadProducts.resetHistory();
@@ -35,7 +30,7 @@ describe('ProductList', () => {
 
     it('should load products if data are too old', done => {
       productList._temp = {
-        get: sinon.stub().returns(Promise.resolve(JSON.stringify(data)))
+        get: sinon.stub().returns(Promise.resolve(JSON.stringify(TEST_DATA.empty)))
       };
 
       productList.init()
@@ -49,19 +44,42 @@ describe('ProductList', () => {
         });
     });
 
-    xit('should take data if available', done => {
-      // TODO: manipulate date
+    it('should take data if available', done => {
+      const clock = sinon.useFakeTimers(+new Date(TEST_DATA.empty.date + ' 05:00:00'));
+      
       productList._temp = {
-        get: sinon.stub().returns(Promise.resolve(JSON.stringify(data)))
+        get: sinon.stub().returns(Promise.resolve(JSON.stringify(TEST_DATA.empty)))
       };
-
+      
       productList.init()
-        .then(result => {
-          productList._temp.get.calledOnce.should.be.true();
-          productList.loadProducts.calledOnce.should.be.false();
+      .then(result => {
+        productList._temp.get.calledOnce.should.be.true();
+        productList.loadProducts.calledOnce.should.be.false();
 
-          done();
-        });
+        result.should.be.true();
+        
+        clock.restore();
+        done();
+      });
+    });
+  });
+
+  describe('parseData', () => {
+    const productList = new ProductList({ client: {} });
+
+    beforeEach(() => {
+      productList._products = [];
+    })
+
+    it('should detect missing data', () => {
+      [ null, undefined, '' ].forEach(data => {
+        productList._parseData(data).should.be.false();
+      });
+    });
+
+    it('should add products from data', () => {
+      productList._parseData(TEST_DATA.parsableProducts).should.be.true();
+      productList._products.should.have.length(4);
     });
   });
 });
